0cb37c578272c9b3fc5ae3168743ea9f25f3992e
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-01-11 10:09:31 +0200

    Create file

---
+++ 1528-ShuffleString/1528-ShuffleString.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun restoreString(s: String, indices: IntArray): String {
+        var stringResult = String()
+        val result = arrayListOf<Char>()
+        s.forEach {
+            result.add(it)
+        }
+        indices.forEachIndexed { index, _ ->
+            result[indices[index]] = s[index]
+        }
+        result.forEach {
+            stringResult += it
+        }
+        return stringResult
+    }
+"
