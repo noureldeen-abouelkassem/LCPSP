6d22dfbbe6f67288ba9d55fbb17e8cf65fb3b20d
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-01-17 11:44:32 +0200

    Update file 1/17/2024, 11:44:32 AM

--- 709-ToLowerCase/709-ToLowerCase.kt
+++ 709-ToLowerCase/709-ToLowerCase.kt
@@ -1,15 +1,6 @@
+class Solution {
     fun toLowerCase(s: String): String {
-        var result = ""
-        val map = hashMapOf<Char, Char>()
-        val upperAlphabetics = ('A'..'Z').toList()
-        val lowerAlphabetics = ('a'..'z').toList()
-        upperAlphabetics.forEachIndexed { index, c ->
-            map[c] = lowerAlphabetics[index]
-        }
-        s.forEach {
-            result += map[it] ?: it
-        }
-        return result
+        return s.lowercase()
     }
 }
 "
