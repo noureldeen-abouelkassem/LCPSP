139443170ee472e73d7df60889dd3ef32ff4cc43
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-01-20 12:54:13 +0200

    Create file

---
+++ 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.kt
@@ -0,0 +1,17 @@
+        var highestNumber = 0
+        candies.forEach {
+            if (highestNumber < it)
+                highestNumber = it
+        }
+        var list = mutableListOf<Boolean>()
+        candies.forEach {
+            if ((it + extraCandies) >= highestNumber) {
+                list.add(true)
+            } else {
+                list.add(false)
+            }
+        }
+        return list
+    }
+}
+[
