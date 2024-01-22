ecdb5ab4c922af022068dd6fbb8f028fb2a613a5
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-01-22 22:26:09 +0200

    Update file 1/22/2024, 10:26:08 PM

--- 1365-HowManyNumbersAreSmallerThantheCurrentNumber/1365-HowManyNumbersAreSmallerThantheCurrentNumber.kt
+++ 1365-HowManyNumbersAreSmallerThantheCurrentNumber/1365-HowManyNumbersAreSmallerThantheCurrentNumber.kt
@@ -1,9 +1,9 @@
 class Solution {
     fun smallerNumbersThanCurrent(nums: IntArray): IntArray {
-        val sorted = nums.distinct().sorted()
+        val sorted = nums.sorted()
         val hashMap = hashMapOf<Int, Int>()
         sorted.forEachIndexed { index, i ->
-            hashMap[i] = index
+                hashMap.putIfAbsent(i, index)
         }
         val result = mutableListOf<Int>()
         nums.forEach {
