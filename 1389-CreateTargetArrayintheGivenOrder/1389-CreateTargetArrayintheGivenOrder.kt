15084f08a822bebffd4e772c22ce2c323751c99e
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-01-30 15:50:06 +0200

    Create file

---
+++ 1389-CreateTargetArrayintheGivenOrder/1389-CreateTargetArrayintheGivenOrder.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun createTargetArray(nums: IntArray, index: IntArray): IntArray {
+        val result = mutableListOf<Int>()
+        index.forEachIndexed { pointer, i ->
+            result.add(i, nums[pointer])
+        }
+        return result.toIntArray()
+    }
+}
+[
