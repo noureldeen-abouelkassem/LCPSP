29863c582c9317e35ff95776df5d3c1b62e4285c
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-01-31 11:54:02 +0200

    Update file 1/31/2024, 11:54:02 AM

--- 1313-DecompressRun-LengthEncodedList/1313-DecompressRun-LengthEncodedList.kt
+++ 1313-DecompressRun-LengthEncodedList/1313-DecompressRun-LengthEncodedList.kt
@@ -1,12 +1,17 @@
 class Solution {
-    fun decompressRLElist(nums: IntArray): IntArray {
-        val result = mutableListOf<Int>()
-        for (i in 0..nums.lastIndex step 2) {
-            repeat(nums[i]){
-                result.add(nums[i+1])
-            }
-        }
-        return result.toIntArray()
+  fun decompressRLElist(nums: IntArray): IntArray =
+    decompress(nums, 0, mutableListOf<Int>()).toIntArray()
+
+fun decompress(nums: IntArray, index: Int, result: MutableList<Int>): List<Int> {
+    
+    if (index >= nums.size) return result
+    
+    repeat(nums[index]) {
+        result.add(nums[index + 1])
     }
+
+    return decompress(nums, index + 2, result)
 }
+}
+
 [
