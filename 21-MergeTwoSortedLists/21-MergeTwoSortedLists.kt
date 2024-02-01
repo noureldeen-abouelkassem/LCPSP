6e1faf55c7998685cffee0c94654c80b86f3727c
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-02-01 13:35:43 +0200

    Update file 2/1/2024, 1:35:42 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.kt
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.kt
@@ -1,16 +1,17 @@
+        if (list1 == null) {
+            return null
         }
-        while (first != null) {
-            val newNode = ListNode(first.`val`)
-            resultTail.next = newNode
-            resultTail = newNode
-            first = first.next
+            return list2
         }
-        while (second != null) {
-            val newNode = ListNode(second.`val`)
-            resultTail.next = newNode
-            resultTail = newNode
-            second = second.next
+        if (list2 == null) {
+            return list1
         }
-        return result
-    }
+        if (list1.`val` < list2.`val`) {
+            list1.next = mergeTwoLists(list1.next, list2)
+            return list1
+        }
+        list2.next = mergeTwoLists(list2.next, list1)
+        return list2
+        if (list1 == null && list2 == null) {
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
 [
