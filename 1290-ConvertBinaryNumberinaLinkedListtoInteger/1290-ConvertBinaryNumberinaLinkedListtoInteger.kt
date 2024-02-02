e87be1c9981916789a24d8c0ea466b5e49abeb78
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-02-02 10:58:31 +0200

    Update file 2/2/2024, 10:58:31 AM

--- 1290-ConvertBinaryNumberinaLinkedListtoInteger/1290-ConvertBinaryNumberinaLinkedListtoInteger.kt
+++ 1290-ConvertBinaryNumberinaLinkedListtoInteger/1290-ConvertBinaryNumberinaLinkedListtoInteger.kt
@@ -1,16 +1,16 @@
- * Definition for singly-linked list.
  * class ListNode(var `val`: Int) {
  *     var next: ListNode? = null
  * }
  */
 class Solution {
     fun getDecimalValue(head: ListNode?): Int {
-        val bits = StringBuilder()
+        var result = 0
         var currentNode = head
         while (currentNode != null) {
-            bits.append(currentNode.`val`)
+            result *= 2
+            result += currentNode.`val`
             currentNode = currentNode.next
         }
-        return bits.toString().toInt(2)
+        return result
     }
 [
