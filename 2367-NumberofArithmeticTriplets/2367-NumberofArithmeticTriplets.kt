dc7b453f10cd14a35a8a1f4fd7a68cd46bde7802
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-02-06 10:04:31 +0200

    Update file 2/6/2024, 10:04:31 AM

--- 2367-NumberofArithmeticTriplets/2367-NumberofArithmeticTriplets.kt
+++ 2367-NumberofArithmeticTriplets/2367-NumberofArithmeticTriplets.kt
@@ -1,16 +1,7 @@
 class Solution {
     fun arithmeticTriplets(nums: IntArray, diff: Int): Int {
-        var count = 0
-        val setNum = nums.toSet()
-        val map = hashMapOf<Int, Int>()
-        setNum.forEach {
-            map[it] = 1
-        }
-        setNum.forEach { 
-            if(map[it.plus(diff)] == 1 && map[it.plus(diff * 2)] == 1)
-                count++
-        }
-        return count
+               return nums.toSet().run { sumBy { it -> if (contains(it + diff) && contains(it + 2*diff)) 1 else 0}}
+
     }
 }
 [
