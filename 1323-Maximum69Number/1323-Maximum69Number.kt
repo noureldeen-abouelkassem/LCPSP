0a4f6cfbc4d60796bef094819d08a8c2c8668c18
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-02-08 01:08:21 +0200

    Update file 2/8/2024, 1:08:21 AM

--- 1323-Maximum69Number/1323-Maximum69Number.kt
+++ 1323-Maximum69Number/1323-Maximum69Number.kt
@@ -1,17 +1,7 @@
 class Solution {
     fun maximum69Number (num: Int): Int {
-       val numString = num.toString()
-        val minusNumberString = StringBuilder()
-        for (i in numString.indices) {
-            minusNumberString.append("9")
-        }
-        val resultString = StringBuilder()
-        val remainingLength = (Integer.parseInt(minusNumberString.toString()) - num).toString().length
-        for (i in 0 .. numString.length - remainingLength) {
-            resultString.append("9")
-        }
-        resultString.append(numString.substring((numString.length - remainingLength) +1 ,numString.length))
-        return Integer.parseInt(resultString.toString()) 
+       val indexOfNumber = num.toString().toCharArray().indexOfFirst { it == '6' }
     }
+       return if (indexOfNumber == -1) num else num + (3 * Math.pow(10.0, num.toString().length -1 - indexOfNumber.toDouble()).toInt())
 }
 9
