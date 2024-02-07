ddfabe13fa6f5229cca666a99458b6764ffa2812
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-02-08 01:09:28 +0200

    Update file 2/8/2024, 1:09:28 AM

--- 1323-Maximum69Number/1323-Maximum69Number.kt
+++ 1323-Maximum69Number/1323-Maximum69Number.kt
@@ -1,7 +1,7 @@
 class Solution {
     fun maximum69Number (num: Int): Int {
-       val indexOfNumber = num.toString().toCharArray().indexOfFirst { it == '6' }
+       val indexOfNumber = num.toString().indexOfFirst { it == '6' }
     }
-       return if (indexOfNumber == -1) num else num + (3 * Math.pow(10.0, num.toString().length -1 - indexOfNumber.toDouble()).toInt())
+        return if (indexOfNumber == -1) num else num + (3 * Math.pow(10.0, num.toString().length -1 - indexOfNumber.toDouble()).toInt())
 }
 9
