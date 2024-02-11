3991ff77af88096b25449542c019709e98ed036e
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-02-11 17:38:43 +0200

    Update file 2/11/2024, 5:38:43 PM

--- 509-FibonacciNumber/509-FibonacciNumber.kt
+++ 509-FibonacciNumber/509-FibonacciNumber.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun fib(n: Int): Int {
-         if (n == 0) return 0
+        if (n == 0) return 0
         if (n == 1) return 1
         return fib(n - 1) + fib(n - 2)
     }
