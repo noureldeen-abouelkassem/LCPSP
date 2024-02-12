43edd02409176668ec658af13e4e959e913ca79b
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-02-12 22:03:44 +0200

    Update file 2/12/2024, 10:03:44 PM

--- 2006-CountNumberofPairsWithAbsoluteDifferenceK/2006-CountNumberofPairsWithAbsoluteDifferenceK.kt
+++ 2006-CountNumberofPairsWithAbsoluteDifferenceK/2006-CountNumberofPairsWithAbsoluteDifferenceK.kt
@@ -1,12 +1,12 @@
 class Solution {
     fun countKDifference(nums: IntArray, k: Int): Int {
-        val hashMap = mutableMapOf<Pair<Int, Int>, IntArray>()
-        nums.forEachIndexed { index, i ->
-            hashMap[Pair(index, i)] = nums.sliceArray(index..nums.lastIndex)
-        }
         var count = 0
-        hashMap.forEach {
-            count += it.value.count { value -> abs(value.minus(it.key.second)) == k }
+        for (i in 0..nums.lastIndex) {
+            for (j in i..nums.lastIndex) {
+                if (abs(nums[i] - nums[j]) == k) {
+                    count++
+                }
+            }
         }
         return count
     }
