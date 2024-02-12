0c30b5970e5e53bcdba6475f3164905e6faa787e
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-02-12 22:17:24 +0200

    Update file 2/12/2024, 10:17:24 PM

--- 2006-CountNumberofPairsWithAbsoluteDifferenceK/2006-CountNumberofPairsWithAbsoluteDifferenceK.kt
+++ 2006-CountNumberofPairsWithAbsoluteDifferenceK/2006-CountNumberofPairsWithAbsoluteDifferenceK.kt
@@ -1,14 +1,13 @@
 class Solution {
     fun countKDifference(nums: IntArray, k: Int): Int {
-        var count = 0
-        for (i in 0..nums.lastIndex) {
-            for (j in i..nums.lastIndex) {
-                if (abs(nums[i] - nums[j]) == k) {
-                    count++
-                }
+        val pairings = nums
+            .groupBy { it - k }
+        val pairsCount = nums
+            .sumOf {
+                pairings.getOrDefault(it, emptyList())
+                    .size
             }
-        }
-        return count
+        return pairsCount
     }
 }
 [
