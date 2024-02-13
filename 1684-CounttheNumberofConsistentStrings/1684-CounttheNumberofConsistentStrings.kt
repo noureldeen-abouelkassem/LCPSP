7cbee9529b12293767f2a88f5e2b4c431cca2155
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-02-13 20:38:14 +0200

    Update file 2/13/2024, 8:38:14 PM

--- 1684-CounttheNumberofConsistentStrings/1684-CounttheNumberofConsistentStrings.kt
+++ 1684-CounttheNumberofConsistentStrings/1684-CounttheNumberofConsistentStrings.kt
@@ -1,9 +1,8 @@
 class Solution {
-    fun countConsistentStrings(allowed: String, words: Array<String>): Int {
+        fun countConsistentStrings(allowed: String, words: Array<String>): Int {
         val allowedSorted = allowed.toCharArray().sorted()
-        return words.count {
-            allowedSorted.containsAll(it.toSet().sorted())
+        return words.map { it.toSet() }.map { it.sorted() }.count { allowedSorted.containsAll(it) }
+
     }
-        }
 }
 "
