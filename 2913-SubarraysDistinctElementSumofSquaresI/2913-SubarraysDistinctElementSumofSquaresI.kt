bf751981c4dee1177bf09e6db7c05b44ac38fab2
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-02-16 14:14:01 +0200

    Update file 2/16/2024, 2:14:01 PM

--- 2913-SubarraysDistinctElementSumofSquaresI/2913-SubarraysDistinctElementSumofSquaresI.kt
+++ 2913-SubarraysDistinctElementSumofSquaresI/2913-SubarraysDistinctElementSumofSquaresI.kt
@@ -1,16 +1,6 @@
 class Solution {
-  fun·sumCounts(nums:·List<Int>):·Int·{
-········val·hashMap·=·hashMapOf<List<Int>,·Int>()
-········nums.forEach·{
-············println(it)
-············hashMap[listOf(it)]·=·hashMap.getOrDefault(listOf(it),·0)·+·1
-········}
-········hashMap[nums]·=·nums.toSet().count().toDouble().pow(2.0).toInt()
-········if(nums.size>2)·{
-············var·lastIndex·=·nums.lastIndex
-············var·startIndex·=·0
-············while·(lastIndex·>·startIndex)·{
-················lastIndex--
-················startIndex++
-················val·rightSub·=·nums.slice(0..lastIndex)
+  fun sumCounts(nums: List<Int>): Int {
+       return (1..nums.size).sumOf { nums.windowed(it).sumOf { window-> window.toSet().size.toDouble().pow(2.0)  }.toInt() }
+    }
+}
 [
