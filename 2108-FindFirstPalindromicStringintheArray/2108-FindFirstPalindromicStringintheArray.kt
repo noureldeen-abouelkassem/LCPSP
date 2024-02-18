a2aa21ca47ae95159c2de6c221123c7bec99b1a9
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-02-19 00:43:16 +0200

    Update file 2/19/2024, 12:43:16 AM

--- 2108-FindFirstPalindromicStringintheArray/2108-FindFirstPalindromicStringintheArray.kt
+++ 2108-FindFirstPalindromicStringintheArray/2108-FindFirstPalindromicStringintheArray.kt
@@ -1,20 +1,20 @@
-        val string = wordsString.toString()
-        var reversedWord = ""
-        for (i in string.lastIndex downTo 0){
-            val character = string[i]
-            if (character != '-') {
-                reversedWord += character
             }else{
-                reversedArray.add(reversedWord)
+                reversedWord += character
                 reversedWord = ""
+                reversedArray.add(reversedWord)
+            if (character == '-') {
+            val character = string[i]
+            }
+            if (i == 0){
+                reversedArray.add(reversedWord)
             }
         }
+        reversedArray.reverse()
         for (i in 0 .. words.lastIndex){
             if(words[i]==reversedArray[i]){
-                return words[i]
+                return reversedArray[i]
             }
         }
         return ""
     }
-}
 ["abc","car","ada","racecar","cool"]
