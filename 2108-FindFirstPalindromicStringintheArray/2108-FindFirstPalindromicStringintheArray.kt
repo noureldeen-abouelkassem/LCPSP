d5c04606f4ede960730b4045cea812818ee7d637
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-02-19 01:32:50 +0200

    Update file 2/19/2024, 1:32:49 AM

--- 2108-FindFirstPalindromicStringintheArray/2108-FindFirstPalindromicStringintheArray.kt
+++ 2108-FindFirstPalindromicStringintheArray/2108-FindFirstPalindromicStringintheArray.kt
@@ -1,6 +1,4 @@
 class Solution {
-    fun firstPalindrome(words: Array<String>): String = words.firstOrNull { w ->
-        (0..w.length / 2).all { w[it] == w[w.lastIndex - it] }
-      } ?: ""
+    fun firstPalindrome(words: Array<String>): String =  words.firstOrNull { it.reversed() == it } ?: ""
 }
 [
