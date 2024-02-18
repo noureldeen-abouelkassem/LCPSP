1aa0d6e5516e632ada4b8d941eb5758bf0f0a9d0
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-02-19 01:17:13 +0200

    Update file 2/19/2024, 1:17:13 AM

--- 2108-FindFirstPalindromicStringintheArray/2108-FindFirstPalindromicStringintheArray.kt
+++ 2108-FindFirstPalindromicStringintheArray/2108-FindFirstPalindromicStringintheArray.kt
@@ -1,20 +1,16 @@
-                word.length / 2 until word.length
+            secondHalf = word.slice(
+                (word.length / 2).inc() until word.length
+            )
+        }
+        return firstHalf == secondHalf.reversed()
+    }
+}
             )
             secondHalf = word.slice(
+                word.length / 2 until word.length
+            )
+        } else {
             firstHalf = word.slice(
                 0 until word.length / 2
-        if (word.length % 2 == 0) {
-        val secondHalf: String
-        val firstHalf: String
-     private fun isWordPalindrome(word: String): Boolean {
-    }
-        return ""
-        }
-            }
-                return word
-            if (isWordPalindrome(word)) {
-        for (word in words) {
-    fun firstPalindrome(words: Array<String>): String {
-class Solution {
-    
+            )
 [
