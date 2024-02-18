04c68b07d5a3423ee4d2f1f526fb27161981594b
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@halan.com>
2024-02-19 00:20:04 +0200

    Update file 2/19/2024, 12:20:03 AM

--- 2108-FindFirstPalindromicStringintheArray/2108-FindFirstPalindromicStringintheArray.kt
+++ 2108-FindFirstPalindromicStringintheArray/2108-FindFirstPalindromicStringintheArray.kt
@@ -1,8 +1,17 @@
-class Solution {
-    fun firstPalindrome(words: Array<String>): String {
-        for(i in words) {
-            if(i.reversed() == i) {
-                return i
+        val string = wordsString.toString()
+        var reversedWord = ""
+        for (i in string.lastIndex downTo 0){
+            val character = string[i]
+            if (character != '-') {
+                reversedWord += character
+            }else{
+                reversedArray.add(reversedWord)
+                reversedWord = ""
+            }
+        }
+        for (i in 0 .. words.lastIndex){
+            if(words[i]==reversedArray[i]){
+                return words[i]
             }
         }
         return ""
