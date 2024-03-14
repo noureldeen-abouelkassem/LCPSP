c849b297f59f92e46ec7aa180831efc3aeae72ad
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-03-14 20:25:13 +0200

    Create file

---
+++ 2864-MaximumOddBinaryNumber/2864-MaximumOddBinaryNumber.kt
@@ -0,0 +1,17 @@
+        }
+        sb.append("1")
+            sb.append("0")
+        repeat(zeros) {
+        }
+                sb.append("1")
+            }
+            repeat(ones - 1) {
+        if (ones > 1) {
+        val sb = StringBuilder(s.length)
+        val zeros = s.length - ones
+        val ones = s.count { ch -> ch == '1' }
+        return sb.toString()
+    }
+    fun maximumOddBinaryNumber(s: String): String {
+class Solution {
+"
