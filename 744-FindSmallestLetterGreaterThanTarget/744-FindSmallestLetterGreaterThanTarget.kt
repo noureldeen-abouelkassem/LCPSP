0dde30bf0e3a932b87540e3972263d85a6a78e66
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-03-16 22:33:41 +0200

    Update file 3/16/2024, 10:33:42 PM

--- 744-FindSmallestLetterGreaterThanTarget/744-FindSmallestLetterGreaterThanTarget.kt
+++ 744-FindSmallestLetterGreaterThanTarget/744-FindSmallestLetterGreaterThanTarget.kt
@@ -1,6 +1,17 @@
 class Solution {
     fun nextGreatestLetter(letters: CharArray, target: Char): Char {
+        var l = 0
+        var r = letters.lastIndex
+        while (l <= r) {
+            val mid = l + (r - l) / 2
+            if (letters[mid] > target) {
+                r = mid - 1
+            } else {
+                l = mid + 1
+            }
+        }
+        val resultIndex = if(l in letters.indices) l else 0
+        return letters[resultIndex]
        }
-        return letters.firstOrNull{ it > target} ?: letters.first()
 }
 [
