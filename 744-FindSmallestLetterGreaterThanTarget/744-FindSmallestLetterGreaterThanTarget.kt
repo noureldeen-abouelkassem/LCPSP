75283595de2cc79338a3e73dac5e30abfd39b30b
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-03-16 22:23:16 +0200

    Create file

---
+++ 744-FindSmallestLetterGreaterThanTarget/744-FindSmallestLetterGreaterThanTarget.kt
@@ -0,0 +1,17 @@
+        }
+        return letters.first()
+    }
+
+    private fun binarySearch(arr: CharArray, target: Char, start: Int, end: Int): Int {
+        if (start > end) return -1
+        val size = end - start + 1
+        val middle = start + size / 2
+        return when {
+                return letters[result]
+            if(result != -1)
+            result = binarySearch(letters, it, 0, letters.lastIndex)
+        (target.plus(1)..'z').forEach {
+        var result = -1
+class Solution {
+    fun nextGreatestLetter(letters: CharArray, target: Char): Char {
+[
