ff5f0cd802fa1c2939dcbe486058637125c8c4d3
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-03-18 21:32:01 +0200

    Update file 3/18/2024, 9:32:01 PM

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.kt
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.kt
@@ -1,11 +1,9 @@
-    private fun findLastOccurrence(nums: IntArray, target: Int): Int {
-        var start = 0
-        var end = nums.size - 1
         var result = -1
 
         while (start <= end) {
             val middle = start + (end - start) / 2
-            if (nums[middle] <= target) {
+            if (nums[middle] == target) result = middle
+            if (nums[middle] < target) {
                 start = middle + 1
             } else {
                 end = middle - 1
@@ -13,6 +11,8 @@
         }
         return result
     }
-            if (nums[middle] == target) result = middle
-}
+        var end = nums.size - 1
+        var start = 0
+    private fun findLastOccurrence(nums: IntArray, target: Int): Int {
+
 [
