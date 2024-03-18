8090e02afe03c28107195a16f9e79f6fdf871bd8
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-03-18 21:14:14 +0200

    Update file 3/18/2024, 9:14:15 PM

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.kt
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.kt
@@ -1,14 +1,17 @@
 
-        while (start <= end) {
-            val middle = start + (end - start) / 2
-            if (nums[middle] <= target) {
-                start = middle + 1
-                if (nums[middle] == target) result = middle
-            } else {
-                end = middle - 1
-            }
-        }
-        return result
     }
-}
+        return result
+        }
+            last = binarySearch(nums, target, last + 1, nums.lastIndex)
+            result[1] = last
+        while (last >= 0) {
+        }
+            first = binarySearch(nums, target, 0, first)
+            result[0] = first
+        while (first >= 0) {
+        var last = first
+        var first = binarySearch(nums, target, 0, nums.lastIndex)
+        val result = intArrayOf(-1,-1)
+    fun searchRange(nums: IntArray, target: Int): IntArray {
+class Solution {
 [
