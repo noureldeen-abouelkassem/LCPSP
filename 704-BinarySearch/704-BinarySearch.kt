aeacda74c8fa6ceb780c09ee863f30fb8202ac83
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-03-19 09:36:04 +0200

    Update file 3/19/2024, 9:36:05 AM

--- 704-BinarySearch/704-BinarySearch.kt
+++ 704-BinarySearch/704-BinarySearch.kt
@@ -1,15 +1,6 @@
 class Solution {
     fun search(nums: IntArray, target: Int): Int {
-        return binarySearch(nums, target, 0, nums.lastIndex)
-    }
-    private fun binarySearch(nums: IntArray, target: Int, start: Int, end: Int) : Int{
-        if(start>end) return  -1
-        val middle = start + (end - start) / 2
-        return when{
-            else -> binarySearch(nums, target, middle.plus(1), end)
-        }
-            nums[middle] == target -> middle
-            nums[middle] > target -> binarySearch(nums, target, start, middle.minus(1))
+         return nums.find { it == target } ?: -1
     }
 }
 [
