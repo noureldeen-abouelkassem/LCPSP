67894fe9a6828ec174fd28bb8ff15f8bb2cd7cdb
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-03-20 15:53:58 +0200

    Update file 3/20/2024, 3:53:58 PM

--- 1351-CountNegativeNumbersinaSortedMatrix/1351-CountNegativeNumbersinaSortedMatrix.kt
+++ 1351-CountNegativeNumbersinaSortedMatrix/1351-CountNegativeNumbersinaSortedMatrix.kt
@@ -1,6 +1,17 @@
-class Solution {
-    fun countNegatives(grid: Array<IntArray>): Int {
-        return grid.sumBy { it.count { it < 0 } }
+        while (start <= end) {
+            val middle = start + (end - start) / 2
+            if(arr[middle] >= 0){
+        var end = arr.lastIndex
+        var start = 0
+    private fun countNegativeNumbers(arr: IntArray): Int{
+
+        return result
     }
-}
+        }
+            println(countNegativeNumbers(it))
+            result += it.size - countNegativeNumbers(it)
+        grid.forEach {
+        var result = 0
+    fun countNegatives(grid: Array<IntArray>): Int {
+class Solution {
 [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
