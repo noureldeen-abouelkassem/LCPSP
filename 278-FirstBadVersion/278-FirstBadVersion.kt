72030312581ef828c3e5d42ca03853d2097579a9
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-03-22 17:51:33 +0200

    Create file

---
+++ 278-FirstBadVersion/278-FirstBadVersion.kt
@@ -0,0 +1,16 @@
+        while (start <= end) {
+            val middle = start + (end - start) / 2
+        }
+            if (isBadVersion(middle)) {
+                end = middle - 1
+            } else {
+            }
+        return start
+                start = middle + 1
+        var end = n
+        var start = 0
+    override fun firstBadVersion(n: Int) : Int {
+class Solution: VersionControl() {
+    }
+}
+5
