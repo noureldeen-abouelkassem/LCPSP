fecd8dc4fee10de8a9b28c4ff71ec82e78f99811
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-03-23 20:13:26 +0200

    Update file 3/23/2024, 8:13:27 PM

--- 69-Sqrt(x)/69-Sqrt(x).kt
+++ 69-Sqrt(x)/69-Sqrt(x).kt
@@ -1,7 +1,7 @@
         var end = num.toLong()
         while (start <= end) {
             val middle = start + (end - start) / 2
-            if (middle * middle >= num) {
+            if (middle * middle > num) {
                 end = middle.minus(1)
             if (middle * middle < num) {
                 start = middle.plus(1)
