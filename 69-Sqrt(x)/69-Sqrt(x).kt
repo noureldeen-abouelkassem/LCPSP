54730ab71ff3597748faea985be302ad0a85218e
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-03-23 19:19:30 +0200

    Create file

---
+++ 69-Sqrt(x)/69-Sqrt(x).kt
@@ -0,0 +1,17 @@
+        var end = num.toLong()
+        while (start <= end) {
+            val middle = start + (end - start) / 2
+            if (middle * middle >= num) {
+                end = middle.minus(1)
+            if (middle * middle < num) {
+                start = middle.plus(1)
+            }
+        }
+            }
+            if(middle * middle == num.toLong()){
+                return middle.toInt()
+            }
+        return end.toInt()
+    }
+}
+4
