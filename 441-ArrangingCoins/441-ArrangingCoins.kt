19fef3fa0abb9f831c7b0c0315575950652d9f2f
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-03-24 10:33:23 +0200

    Create file

---
+++ 441-ArrangingCoins/441-ArrangingCoins.kt
@@ -0,0 +1,18 @@
+                start = middle.plus(1)
+                end = middle.minus(1)
+            }
+            if (sumOfNumbersBefore < n) {
+            if (sumOfNumbersBefore > n) {
+            }
+                return middle
+            }
+        }
+        return end
+    }
+
+    private fun sumOfNumbersBefore(n: Int): Int {
+        return n * (n + 1) / 2
+    }
+            if(sumOfNumbersBefore == n){
+            val sumOfNumbersBefore = sumOfNumbersBefore(middle)
+5
