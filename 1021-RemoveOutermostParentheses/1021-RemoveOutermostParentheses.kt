0c3fa1e707b0bcf0ccc74e1429ac4b4337fd1471
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-03-29 16:50:19 +0200

    Create file

---
+++ 1021-RemoveOutermostParentheses/1021-RemoveOutermostParentheses.kt
@@ -0,0 +1,13 @@
+            } else {
+                openParenthesesCounter --
+                openParenthesesCounter ++
+                }
+                    startPointer = index
+                if(openParenthesesCounter == 0){
+            if(c == '('){
+        s.forEachIndexed { index, c ->
+        val newString = StringBuilder()
+        var startPointer = 0
+        var openParenthesesCounter = 0
+    fun removeOuterParentheses(s: String): String {
+"
