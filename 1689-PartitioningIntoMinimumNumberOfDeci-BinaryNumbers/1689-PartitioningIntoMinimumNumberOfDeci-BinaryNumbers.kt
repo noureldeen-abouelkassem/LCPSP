df843bd7439e30ee57ddd1d1de39910af7c50865
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-03-31 10:02:51 +0200

    Update file 3/31/2024, 10:02:53 AM

--- 1689-PartitioningIntoMinimumNumberOfDeci-BinaryNumbers/1689-PartitioningIntoMinimumNumberOfDeci-BinaryNumbers.kt
+++ 1689-PartitioningIntoMinimumNumberOfDeci-BinaryNumbers/1689-PartitioningIntoMinimumNumberOfDeci-BinaryNumbers.kt
@@ -1,12 +1,10 @@
-        var start = 0
-    private fun binarySearch(array: String): Char {
-
-    }
-       var num  = 0
-    fun minPartitions(n: String): Int {
-class Solution {
+       var num  = '0'
             n.forEach {
-                if (it - '0' > num) num = it - '0'
+                if (it > num) num = it
             }
         return num
+    }
+}
+    fun minPartitions(n: String): Int {
+class Solution {
 "
