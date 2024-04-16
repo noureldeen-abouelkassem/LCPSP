b13b609509a363253ceeb0bf90b68d0fcc37461f
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-04-16 09:54:20 +0200

    Update file 4/16/2024, 9:54:20 AM

--- 3110-ScoreofaString/3110-ScoreofaString.kt
+++ 3110-ScoreofaString/3110-ScoreofaString.kt
@@ -1,11 +1,8 @@
 class Solution {
     fun scoreOfString(s: String): Int {
-        var result = 0
-        s.map { it.code }.reduce { acc, i ->
-             result += abs(acc - i)
-             i
-         }
-        return result
+        return s.zipWithNext { a, b ->
+            abs(a - b)
+        }.sum()
     }
 }
 "
