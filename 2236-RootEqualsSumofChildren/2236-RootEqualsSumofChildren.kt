8fc8ddc7859910cfa818de94cddaacf218add163
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-04-19 16:41:35 +0200

    Create file

---
+++ 2236-RootEqualsSumofChildren/2236-RootEqualsSumofChildren.kt
@@ -0,0 +1,16 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun checkTree(root: TreeNode?): Boolean {
+        return root?.`val` == (root?.right?.`val`?.let { root.left?.`val`?.plus(it) } ?: false)
+    }
+}
+[
