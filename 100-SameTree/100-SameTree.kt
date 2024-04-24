6a766883236bca0558abdeee60f117ea795619c7
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-04-24 23:58:25 +0200

    Create file

---
+++ 100-SameTree/100-SameTree.kt
@@ -0,0 +1,17 @@
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        var ind = true
+        if (p==null && q==null) return true
+        if (p==null && q!=null || p!=null && q==null) return false
+        if (p!!.`val` != q!!.`val`) return false
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+        if (p.left==null && q.left!=null || p.left!=null && q.left==null) return false
+        if (p.right==null && q.right!=null || p.right!=null && q.right==null) return false
+        return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right))
+    }
+[
