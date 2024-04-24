888a5fdea66076f0a97f8163104a31ed6b2b6579
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-04-25 00:01:05 +0200

    Update file 4/25/2024, 12:01:04 AM

--- 100-SameTree/100-SameTree.kt
+++ 100-SameTree/100-SameTree.kt
@@ -4,7 +4,6 @@
  */
 class Solution {
     fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
-        var ind = true
         if (p==null && q==null) return true
         if (p==null && q!=null || p!=null && q==null) return false
         if (p!!.`val` != q!!.`val`) return false
@@ -14,4 +13,5 @@ class Solution {
         if (p.right==null && q.right!=null || p.right!=null && q.right==null) return false
         return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right))
     }
+}
 [
