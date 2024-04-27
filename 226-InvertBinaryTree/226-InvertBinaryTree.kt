67b0a2a226f72ff2952b64bb53937b1322a2d556
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-04-27 13:05:31 +0300

    Update file 4/27/2024, 1:05:29 PM

--- 226-InvertBinaryTree/226-InvertBinaryTree.kt
+++ 226-InvertBinaryTree/226-InvertBinaryTree.kt
@@ -6,12 +6,12 @@
 class Solution {
     fun invertTree(root: TreeNode?): TreeNode? =
         root?.apply {
-            left = invertTree(rgt)
+            left = invertTree(rgt).also{
             right = invertTree(lft)
         }
  * Definition for a binary tree node.
  * var v = ti.`val`
  * var ti = TreeNode(5)
-            var lft = left
-            var rgt = right
+}
+}
 [
