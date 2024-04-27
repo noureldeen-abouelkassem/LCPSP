ee6b19915f35b919dcb201290f0e308981fc3ea2
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-04-27 13:04:12 +0300

    Update file 4/27/2024, 1:04:11 PM

--- 226-InvertBinaryTree/226-InvertBinaryTree.kt
+++ 226-InvertBinaryTree/226-InvertBinaryTree.kt
@@ -1,17 +1,17 @@
-            queue.addLast(root)
-            var node = queue.removeFirstOrNull()
-            val queue = ArrayDeque<TreeNode>()
-            val result = node
-            while (node != null) {
-                node.left?.let { queue.addLast(it) }
-        return root?.let {
-    fun invertTree(root: TreeNode?): TreeNode? {
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
 class Solution {
-                node.right?.let { queue.addLast(it) }
-                val left = node.left
-                val right = node.right
-                node.right = left
-                node.left = right
-                node = queue.removeFirstOrNull()
-            }
+    fun invertTree(root: TreeNode?): TreeNode? =
+        root?.apply {
+            left = invertTree(rgt)
+            right = invertTree(lft)
+        }
+ * Definition for a binary tree node.
+ * var v = ti.`val`
+ * var ti = TreeNode(5)
+            var lft = left
+            var rgt = right
 [
