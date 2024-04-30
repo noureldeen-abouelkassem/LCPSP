280e49986e140ddacc5e8d8c0183dc516d80e588
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-04-30 15:56:59 +0300

    Create file

---
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.kt
@@ -0,0 +1,17 @@
+        while (start <= end) {
+        var end = arr.lastIndex
+            val middle = start + (end - start) / 2
+        var start = 0
+    private fun binarySearch(arr: IntArray, target: Int): Boolean {
+
+    }
+        return result
+        }
+            arrayIndex++
+            result = binarySearch(matrix[arrayIndex], target)
+        while (result.not() && arrayIndex < matrix.size) {
+        var arrayIndex = 0
+        var result = false
+    fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
+class Solution {
+[
