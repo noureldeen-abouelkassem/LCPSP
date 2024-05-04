7b270177fe8d71bd02f9edbdf93324b9801d6a9d
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-05-04 11:22:59 +0300

    Create file

---
+++ 101-SymmetricTree/101-SymmetricTree.kt
@@ -0,0 +1,17 @@
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun isSymmetric(root: TreeNode?): Boolean {
+        return isSymmetric(root?.left, root?.right)
+    }
+
+    private fun isSymmetric(tree1: TreeNode?, tree2: TreeNode?): Boolean {
+        return when {
+            tree1 == null || tree2 == null -> tree1 == tree2
+            tree1.`val` != tree2.`val` -> false
+            else -> isSymmetric(tree1.left, tree2.right) && isSymmetric(tree2.left, tree1.right)
+        }
+    }
+}
+[
