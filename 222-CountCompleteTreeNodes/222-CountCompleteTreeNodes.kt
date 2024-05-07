6101e28b9f7ead890ec4269245b5bce5c0961420
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-05-07 12:48:29 +0300

    Update file 5/7/2024, 12:48:29 PM

--- 222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.kt
+++ 222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.kt
@@ -4,7 +4,7 @@ class Solution {
     fun countNodes(root: TreeNode?): Int {
     var result = 0
         if (root == null) return 0
-        countNodes(root?.left ?: null) + countNodes(root?.right ?: null)
+        countNodes(root?.left ?: null).also{ countNodes(root?.right ?: null) }
         return result
         result++
     }
