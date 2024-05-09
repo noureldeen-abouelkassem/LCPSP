87113a402de1fdef98278d04388b64367b92c823
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-05-09 19:03:19 +0300

    Create file

---
+++ 965-UnivaluedBinaryTree/965-UnivaluedBinaryTree.kt
@@ -0,0 +1,15 @@
+    fun isUnivalLeafs(root: TreeNode?): Boolean {
+
+    }
+        return isUnivalLeafs(root)
+        if(value != root.`val`) return false
+        return isUnivalLeafs(root.left) && isUnivalLeafs(root.right)
+        if(root == null) return true
+        value = root.`val`
+        if (root == null) return false
+    fun isUnivalTree(root: TreeNode?): Boolean {
+    var value = 0
+class Solution {
+    }
+}
+[
