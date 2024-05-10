1a84269126db4915f6905fc9d6d9b548982de03a
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-05-10 11:08:59 +0300

    Update file 5/10/2024, 11:09:01 AM

--- 872-Leaf-SimilarTrees/872-Leaf-SimilarTrees.kt
+++ 872-Leaf-SimilarTrees/872-Leaf-SimilarTrees.kt
@@ -1,6 +1,6 @@
         if (root.left == null && root.right == null)
             root1Leafs.addLast(root.`val`)
-        node1Leafs(root.left).also { node1Leafs(root.right) }
+        node1Leafs(root.left)
         if (root == null) return
     private fun node1Leafs(root: TreeNode?) {
 
@@ -10,7 +10,9 @@
         if (root == null) return
         if (root.left == null && root.right == null)
             root2Leafs.addLast(root.`val`)
-        node2Leafs(root.left).also { node2Leafs(root.right) }
+        node2Leafs(root.left)
     }
+        node2Leafs(root.right)
+        node1Leafs(root.right)
 }
 [
