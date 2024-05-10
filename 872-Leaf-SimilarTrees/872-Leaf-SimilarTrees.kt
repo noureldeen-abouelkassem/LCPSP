73bd63a0f13f97905baae8b2b5b96396f167fe29
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-05-10 11:00:26 +0300

    Update file 5/10/2024, 11:00:28 AM

--- 872-Leaf-SimilarTrees/872-Leaf-SimilarTrees.kt
+++ 872-Leaf-SimilarTrees/872-Leaf-SimilarTrees.kt
@@ -1,17 +1,18 @@
-       leafNodes(root2, leaves2)
-       val leaves2 = mutableListOf<Int>()
-       leafNodes(root1, leaves1)
-       val leaves1 = mutableListOf<Int>()
+        var result = true
+        if(root1Leafs.size != root2Leafs.size) return false
+    private fun checkIdentical(root1Leafs: ArrayDeque<Int>, root2Leafs: ArrayDeque<Int>): Boolean {
+
+    }
+        return checkIdentical(root1Leafs, root2Leafs)
+        root1Leafs.forEachIndexed { index, i -> 
+            if(root2Leafs.getOrNull(index) != i)
+                result = false
+        }
+        return result
+        node1Leafs(root1).also{node2Leafs(root2)}
+        if (root1 == null || root2 == null) return false
     fun leafSimilar(root1: TreeNode?, root2: TreeNode?): Boolean {
+    private var root2Leafs = ArrayDeque<Int>()
+    private var root1Leafs = ArrayDeque<Int>()
 class Solution {
- */
- * }
- *     var right: TreeNode? = null
- *     var left: TreeNode? = null
- * class TreeNode(var `val`: Int) {
- * Definition for a binary tree node.
- * var v = ti.`val`
- * var ti = TreeNode(5)
- * Example:
-/**
 [
