5e4aee420e8a146e914002a0571a0b3d5e7e2b93
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-05-10 11:08:40 +0300

    Update file 5/10/2024, 11:08:42 AM

--- 872-Leaf-SimilarTrees/872-Leaf-SimilarTrees.kt
+++ 872-Leaf-SimilarTrees/872-Leaf-SimilarTrees.kt
@@ -1,18 +1,16 @@
         if (root.left == null && root.right == null)
             root1Leafs.addLast(root.`val`)
-        node1Leafs(root.left)
+        node1Leafs(root.left).also { node1Leafs(root.right) }
         if (root == null) return
     private fun node1Leafs(root: TreeNode?) {
 
     }
-        return root1Leafs == root2Leafs
-        node1Leafs(root1)
-        if (root1 == null || root2 == null) return false
-    fun leafSimilar(root1: TreeNode?, root2: TreeNode?): Boolean {
-    private var root2Leafs = ArrayDeque<Int>()
-    private var root1Leafs = ArrayDeque<Int>()
-class Solution {
- */
- * }
-        node2Leafs(root2)
+
+    private fun node2Leafs(root: TreeNode?) {
+        if (root == null) return
+        if (root.left == null && root.right == null)
+            root2Leafs.addLast(root.`val`)
+        node2Leafs(root.left).also { node2Leafs(root.right) }
+    }
+}
 [
