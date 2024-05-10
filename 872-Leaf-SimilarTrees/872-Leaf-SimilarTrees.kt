39cf680f3eff5fb1009d6638b7fab5b1bddecbfa
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-05-10 11:03:59 +0300

    Update file 5/10/2024, 11:04:01 AM

--- 872-Leaf-SimilarTrees/872-Leaf-SimilarTrees.kt
+++ 872-Leaf-SimilarTrees/872-Leaf-SimilarTrees.kt
@@ -1,18 +1,18 @@
-        var result = true
-        if(root1Leafs.size != root2Leafs.size) return false
-    private fun checkIdentical(root1Leafs: ArrayDeque<Int>, root2Leafs: ArrayDeque<Int>): Boolean {
 
+    private fun node1Leafs(root: TreeNode?) {
     }
-        return checkIdentical(root1Leafs, root2Leafs)
-        root1Leafs.forEachIndexed { index, i -> 
-            if(root2Leafs.getOrNull(index) != i)
-                result = false
-        }
-        return result
-        node1Leafs(root1).also{node2Leafs(root2)}
+        return root1Leafs == root2Leafs
+        node1Leafs(root1).also{ node2Leafs(root2) }
         if (root1 == null || root2 == null) return false
+        if (root == null) return
+        if (root.left == null && root.right == null)
+            root1Leafs.addLast(root.`val`)
+        node1Leafs(root.left).also { node1Leafs(root.right) }
+    }
+
     fun leafSimilar(root1: TreeNode?, root2: TreeNode?): Boolean {
     private var root2Leafs = ArrayDeque<Int>()
     private var root1Leafs = ArrayDeque<Int>()
 class Solution {
+ */
 [
