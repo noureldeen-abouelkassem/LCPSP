c5ee13e6ef1f1d3aac6c04cfb01bde31bf9c6078
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-05-13 17:33:54 +0300

    Update file 5/13/2024, 5:33:54 PM

--- 3131-FindtheIntegerAddedtoArrayI/3131-FindtheIntegerAddedtoArrayI.kt
+++ 3131-FindtheIntegerAddedtoArrayI/3131-FindtheIntegerAddedtoArrayI.kt
@@ -4,11 +4,11 @@ class Solution {
         var num2 = nums2[0]
         var pointer = 1
         while(pointer <= nums1.lastIndex){
-            num2 -= nums2[pointer]
+            num2 += nums2[pointer]
             num1 += nums1[pointer]
             pointer++
         }
-        return (num2 + num1) / nums1.size
+        return (num2 - num1) / nums1.size
     }
         if(nums1.size == 1) return nums2[0] - nums1[0]
 }
