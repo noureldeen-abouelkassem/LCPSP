bebfc425fccf2b37db2df53c78699d28a4933807
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-05-13 17:31:32 +0300

    Update file 5/13/2024, 5:31:32 PM

--- 3131-FindtheIntegerAddedtoArrayI/3131-FindtheIntegerAddedtoArrayI.kt
+++ 3131-FindtheIntegerAddedtoArrayI/3131-FindtheIntegerAddedtoArrayI.kt
@@ -1,6 +1,15 @@
 class Solution {
     fun addedInteger(nums1: IntArray, nums2: IntArray): Int {
+        var num1 = nums1[0]
+        var num2 = nums2[0]
+        var pointer = 1
+        while(pointer <= nums1.lastIndex){
+            num2 -= nums2[pointer]
+            num1 += nums1[pointer]
+            pointer++
+        }
+        return (num2 + num1) / nums1.size
     }
-        return (nums2.sum() - nums1.sum()) / nums1.size
+        if(nums1.size == 1) return nums2[0] - nums1[0]
 }
 [
