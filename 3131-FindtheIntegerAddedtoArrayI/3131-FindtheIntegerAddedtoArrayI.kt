d85835fa85f53da7ada9ca15b52b8ca6ea096572
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-05-13 17:34:07 +0300

    Update file 5/13/2024, 5:34:08 PM

--- 3131-FindtheIntegerAddedtoArrayI/3131-FindtheIntegerAddedtoArrayI.kt
+++ 3131-FindtheIntegerAddedtoArrayI/3131-FindtheIntegerAddedtoArrayI.kt
@@ -1,6 +1,5 @@
 class Solution {
     fun addedInteger(nums1: IntArray, nums2: IntArray): Int {
-        var num1 = nums1[0]
         var num2 = nums2[0]
         var pointer = 1
         while(pointer <= nums1.lastIndex){
@@ -10,6 +9,6 @@ class Solution {
         }
         return (num2 - num1) / nums1.size
     }
-        if(nums1.size == 1) return nums2[0] - nums1[0]
+        var num1 = nums1[0]
 }
 [
