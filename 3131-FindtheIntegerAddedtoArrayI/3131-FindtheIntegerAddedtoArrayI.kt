e056f6b9b361b1fcdd3c64b70b744f3b74b1a783
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-05-13 17:12:54 +0300

    Update file 5/13/2024, 5:12:55 PM

--- 3131-FindtheIntegerAddedtoArrayI/3131-FindtheIntegerAddedtoArrayI.kt
+++ 3131-FindtheIntegerAddedtoArrayI/3131-FindtheIntegerAddedtoArrayI.kt
@@ -1,9 +1,6 @@
 class Solution {
     fun addedInteger(nums1: IntArray, nums2: IntArray): Int {
-        val difference = Math.abs(nums1Sorted[0] - nums2Sorted[0])
     }
-        return if ((nums1Sorted[0] + difference) == nums2Sorted[0]) difference else (difference * -1)
-        val nums1Sorted = nums1.sorted()
-        val nums2Sorted = nums2.sorted()
+        return (nums2.sum() - nums1.sum()) / nums1.size
 }
 [
