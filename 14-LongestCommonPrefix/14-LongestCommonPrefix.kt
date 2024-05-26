3849a1ef582720e2bb1cde742f61a3965b8b1512
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-05-26 17:37:10 +0300

    Update file 5/26/2024, 5:37:10 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.kt
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.kt
@@ -1,10 +1,16 @@
 class Solution {
      fun longestCommonPrefix(strs: Array<String>): String {
         var result = strs[0]
-        result = strs.drop(1).fold(result){ int , acc ->
-             int.commonPrefixWith(acc)
+        result = strs.drop(1).fold(result) { int, acc ->
+            val stringBuilder = StringBuilder()
+            var counter = 0
+                stringBuilder.append(acc[counter])
+                counter++
+            }
+            stringBuilder.toString()
         }
         return result
     }
+             while (int.getOrNull(counter) == acc.getOrNull(counter)) {
 }
 [
