3c204347ee2bf5e5ff2ec1e851de17c0abf4fb3c
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-05-27 20:57:37 +0300

    Update file 5/27/2024, 8:57:38 PM

--- 47-PermutationsII/47-PermutationsII.kt
+++ 47-PermutationsII/47-PermutationsII.kt
@@ -1,23 +1,23 @@
-        for (i in numsHash.keys) {
-            if (numsHash[i] == 0) {
-                continue
             }
-            prefix.add(i)
-            numsHash[i] = numsHash.getOrDefault(i, 1).minus(1)
-        val result = mutableListOf<List<Int>>()
-    private fun permuteRec(numsHash: HashMap<Int, Int>, prefix: MutableList<Int>): List<List<Int>> {
-
-    }
-        return permuteRec(hashMap, mutableListOf())
-        }
-            result.addAll(permuteRec(numsHash, prefix))
-            numsHash[i] = numsHash.getOrDefault(i, 0).plus(1)
+            prefix.add(numsHash.elementAt(i))
+            takken[i] = true
+            uniqueSet.add(numsHash.elementAt(i))
+            result.addAll(permuteRec(numsHash, takken, prefix))
+            takken[i] = false
             prefix.removeLast()
         }
         if (result.isEmpty()) {
             val newPrefixList = mutableListOf<Int>()
             newPrefixList.addAll(prefix)
+                continue
+        for (i in 0..<numsHash.size) {
+            if (takken[i] || uniqueSet.contains(numsHash.elementAt(i))) {
+        val result = mutableListOf<List<Int>>()
+        val uniqueSet = HashSet<Int>()
+    private fun permuteRec(numsHash: IntArray, takken: BooleanArray, prefix: MutableList<Int>): List<List<Int>> {
             result.add(newPrefixList)
         }
         return result
+    }
+}
 [
