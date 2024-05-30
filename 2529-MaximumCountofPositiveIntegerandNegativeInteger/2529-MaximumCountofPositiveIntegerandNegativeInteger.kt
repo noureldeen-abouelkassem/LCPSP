ce0691f8ae467718446b896eddf24ba5fde5642e
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-05-30 20:38:58 +0300

    Update file 5/30/2024, 8:38:59 PM

--- 2529-MaximumCountofPositiveIntegerandNegativeInteger/2529-MaximumCountofPositiveIntegerandNegativeInteger.kt
+++ 2529-MaximumCountofPositiveIntegerandNegativeInteger/2529-MaximumCountofPositiveIntegerandNegativeInteger.kt
@@ -1,6 +1,22 @@
-class Solution {
+    private fun binarySearch(nums: IntArray, target: Int): Int {
+        var start = 0
+        var end = nums.lastIndex
+        while (start <= end){
+            val middle = start + (end - start) / 2
+                start = middle + 1
+            } else {
+                end = middle - 1
+            }
+        }
+           if(nums[middle] < target){
+
+    }
+        return maxOf(positive, negative)
+        val negative = binarySearch(nums, 0)
+        val positive = nums.size - binarySearch(nums, 1)
     fun maximumCount(nums: IntArray): Int {
-        return maxOf(nums.filter { it > 0 }.size, nums.filter { it < 0 }.size)
+class Solution {
+        return start
     }
 }
 [
