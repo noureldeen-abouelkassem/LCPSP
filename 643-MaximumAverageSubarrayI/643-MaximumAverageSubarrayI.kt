e84224d1e0940a33cc0ab4e581ad208162877154
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-06-01 10:00:07 +0300

    Update file 6/1/2024, 10:00:06 AM

--- 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.kt
+++ 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.kt
@@ -1,12 +1,20 @@
 class Solution {
     fun findMaxAverage(nums: IntArray, k: Int): Double {
-        var result :Double = Double.NEGATIVE_INFINITY
-        nums.toList().windowed(k){ subNums  ->
-            if(subNumsAverage > result)
-                result = subNumsAverage
+        var windowSum = 0
+        var windowStart = 0
+        var result = Double.NEGATIVE_INFINITY
+        for (windowEnd in 0..nums.lastIndex) {
+            windowSum += nums[windowEnd]
+            if (windowEnd >= k.minus(1)) {
+                val average = windowSum.div(k * 1.0)
+                if (average > result) {
+                    result = average
+                }
+                windowSum -= nums[windowStart]
+                windowStart++
+            }
         }
         return result
     }
-            val subNumsAverage : Double = subNums.sum().div(k.toDouble())
 }
 [
