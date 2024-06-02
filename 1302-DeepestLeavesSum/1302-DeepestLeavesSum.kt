fd534a00813360ae4d06f9481186e66ddf3d01ad
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-06-02 19:43:02 +0300

    Create file

---
+++ 1302-DeepestLeavesSum/1302-DeepestLeavesSum.kt
@@ -0,0 +1,18 @@
+                        arrayQueue.add(it)
+                    }
+                    it.right?.let{
+                        arrayQueue.add(it)
+                    it.left?.let{
+                    sum += it.`val`
+                node?.let{
+                val node = arrayQueue.removeFirstOrNull()
+            while(nodesInCurrentLevel > 0){
+            var sum = 0
+            nodesInCurrentLevel = arrayQueue.size
+        while(arrayQueue.isNotEmpty()) {
+        root?.let{ arrayQueue.addLast(it) }
+        var nodesInCurrentLevel = 0
+        var arrayQueue = ArrayDeque<TreeNode>()
+        var sumArray = mutableListOf<Int>()
+    fun deepestLeavesSum(root: TreeNode?): Int {
+[
