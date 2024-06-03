b7329f1e16542b21e1f4fd80dfe8bb46c3764842
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-06-03 12:44:13 +0300

    Update file 6/3/2024, 12:44:12 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.kt
+++ 217-ContainsDuplicate/217-ContainsDuplicate.kt
@@ -1,12 +1,6 @@
 class Solution {
     fun containsDuplicate(nums: IntArray): Boolean {
-        val sortedNums = nums.sorted()
-        for(i in 1..nums.lastIndex) {
-            if(sortedNums[i.minus(1)] == sortedNums[i]){
-                return true
-            }
-        }
+        return nums.toSet().size != nums.size
     }
-        return false
 }
 [
