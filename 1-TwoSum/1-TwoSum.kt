4ac00c684a783ca2843f87986ae8ff5b72c53412
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-06-03 18:14:29 +0300

    Update file 6/3/2024, 6:14:29 PM

--- 1-TwoSum/1-TwoSum.kt
+++ 1-TwoSum/1-TwoSum.kt
@@ -1,23 +1,16 @@
-        nums.forEachIndexed { index, number ->
-            hashMap[number] = index
-        }
-        val hashMapSorted = hashMap.toSortedMap()
-        val hashMapKeys = hashMapSorted.keys
-        var left = 0
-        var right = hashMapKeys.size.minus(1)
-
-        while (left <= right) {
             when {
-                hashMapKeys.elementAt(left) + hashMapKeys.elementAt(right) > target -> right--
-                hashMapKeys.elementAt(left) + hashMapKeys.elementAt(right) == target -> return intArrayOf(hashMapSorted
-
-                hashMapKeys.elementAt(left) + hashMapKeys.elementAt(right)  < target -> left++
             }
         }
+                left + right == target -> return intArrayOf(nums.indexOfFirst{it == left}, nums.indexOfLast
+                left + right > target -> rightPointer--
+            val right = numsSorted[rightPointer]
+                left + right < target -> leftPointer++
+            println(left)
+            println(right)
         return intArrayOf()
-[hashMapKeys.elementAt(left)] ?: 0, hashMapSorted[hashMapKeys.elementAt(right)] ?: 0)
-        val hashMap = hashMapOf<Int, Int>()
-        if (nums.sum() == target) return (0..nums.lastIndex).toList().toIntArray()
-    fun twoSum(nums: IntArray, target: Int): IntArray {
-class Solution {
+    }
+            val left = numsSorted[leftPointer]
+        while(leftPointer < rightPointer){
+{it == right})
+}
 [
