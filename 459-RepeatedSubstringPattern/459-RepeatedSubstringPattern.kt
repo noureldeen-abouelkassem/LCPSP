874544245547a638c48b755b47fb22e4f29ba232
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-06-07 14:26:35 +0300

    Update file 6/7/2024, 2:26:36 PM

--- 459-RepeatedSubstringPattern/459-RepeatedSubstringPattern.kt
+++ 459-RepeatedSubstringPattern/459-RepeatedSubstringPattern.kt
@@ -1,12 +1,12 @@
+class Solution {
+    fun repeatedSubstringPattern(s: String): Boolean {
+        if (s.length <= 1) return false
+        if (s.toSet().size == 1) return true
+        var windowEnd = 1
+        while (windowEnd < s.lastIndex) {
+            if(s.split(s.substring(windowEnd.plus(1)..s.lastIndex)).any { it.isNotEmpty() }){
+                windowEnd++
             } else {
                 return true
             }
-                windowEnd++
-            if(s.split(s.substring(0..windowEnd)).any { it.isNotEmpty() }){
-        while (windowEnd < s.lastIndex) {
-        var windowEnd = 1
-        if (s.length <= 1) return false
-    fun repeatedSubstringPattern(s: String): Boolean {
-class Solution {
-        if (s.toSet().size == 1) return true
 "
