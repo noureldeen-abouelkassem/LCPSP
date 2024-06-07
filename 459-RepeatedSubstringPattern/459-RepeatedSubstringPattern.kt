0f6bc81f04044fcd5c89e33f81ee6f8fb943fe5f
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-06-07 14:09:21 +0300

    Create file

---
+++ 459-RepeatedSubstringPattern/459-RepeatedSubstringPattern.kt
@@ -0,0 +1,12 @@
+            } else {
+                return true
+            }
+                windowEnd++
+            if(s.split(s.substring(0..windowEnd)).any { it.isNotEmpty() }){
+        while (windowEnd < s.lastIndex) {
+        var windowEnd = 1
+        if (s.length <= 1) return false
+    fun repeatedSubstringPattern(s: String): Boolean {
+class Solution {
+        if (s.toSet().size == 1) return true
+"
