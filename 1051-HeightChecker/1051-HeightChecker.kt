7b0be9ae282f77f6535c87dc393b153c0df83fe0
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-06-10 14:36:10 +0300

    Create file

---
+++ 1051-HeightChecker/1051-HeightChecker.kt
@@ -0,0 +1,12 @@
+        for (i in 0 .. heights.lastIndex) {
+            if(sorted[i] != heights[i]){
+            }
+        }
+    }
+                result += 1
+        var sorted = heights.sorted()
+        var result = 0 
+    fun heightChecker(heights: IntArray): Int {
+        return result
+}
+[
