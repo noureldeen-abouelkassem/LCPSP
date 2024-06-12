0fa08308d772733b43b16d49e49d3a8cbb633205
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-06-12 11:31:48 +0300

    Create file

---
+++ 75-SortColors/75-SortColors.kt
@@ -0,0 +1,10 @@
+        hashMap.forEach { number->
+        var index = 0
+            repeat(number.value){
+                nums[index] = number.key
+                index ++
+            }
+        }
+    }
+}
+[
