42c8c8a1539b650ce7dccbc6895304371ac1f3b1
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-06-13 21:18:44 +0300

    Update file 6/13/2024, 9:18:45 PM

--- 2235-AddTwoIntegers/2235-AddTwoIntegers.kt
+++ 2235-AddTwoIntegers/2235-AddTwoIntegers.kt
@@ -1,4 +1,13 @@
 class Solution {
-    fun sum(num1: Int, num2: Int): Int = num1 + num2
+    fun sum(num1: Int, num2: Int): Int {
+        var x = num1
+        var y = num2
+        while (y != 0) {
+             val carry = x and y
+             x = x xor y
+             y = carry shl 1
+         }
+        return x
+    }
 }
 1
