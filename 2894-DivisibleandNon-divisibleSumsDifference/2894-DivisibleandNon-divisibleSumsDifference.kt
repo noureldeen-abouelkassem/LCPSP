7a3e5390032a26c174a0a27841633604cdcf55cf
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-06-19 14:14:59 +0300

    Create file

---
+++ 2894-DivisibleandNon-divisibleSumsDifference/2894-DivisibleandNon-divisibleSumsDifference.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun differenceOfSums(n: Int, m: Int): Int {
+        var num1 = 0
+        var num2 = 0 
+        for(i in 1..n){
+            if(i.mod(m) != 0) {
+            }
+        }
+                num1 += i
+            if(i.mod(m) == 0) {
+            }
+    }
+                num2 += i
+        return num1 - num2
+}
+1
