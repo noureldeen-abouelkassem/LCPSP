2132f836334da4b8d8346ddb5827d4c2362a6920
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-06-23 14:49:39 +0300

    Create file

---
+++ 2807-InsertGreatestCommonDivisorsinLinkedList/2807-InsertGreatestCommonDivisorsinLinkedList.kt
@@ -0,0 +1,17 @@
+            workingNode = nextNode
+        }
+        return head
+    }
+}
+
+fun calculateGCD(a: Int, b: Int): Int {
+    var num1 = a
+    var num2 = b
+    while (num2 != 0) {
+        val temp = num2
+        num2 = num1 % num2
+        num1 = temp
+    }
+    return num1
+}
+[
