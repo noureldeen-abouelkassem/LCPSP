2bd9caa988e665f957f017bbd2c12f07b7172478
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-07-05 11:21:08 +0300

    Create file

---
+++ 16-3SumClosest/16-3SumClosest.kt
@@ -0,0 +1,18 @@
+                }
+                when {
+                    closestSum = currentSum
+                if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+                val currentSum = nums[i] + nums[left] + nums[right]
+            while (left < right) {
+            var right = nums.size - 1
+            var left = i + 1
+        for (i in nums.indices) {
+        var closestSum = nums[0] + nums[1] + nums[2]
+        nums.sort()
+    fun threeSumClosest(nums: IntArray, target: Int): Int {
+class Solution {
+                    currentSum < target -> left++
+                    currentSum > target -> right--
+                    else -> return currentSum 
+                }
+[
