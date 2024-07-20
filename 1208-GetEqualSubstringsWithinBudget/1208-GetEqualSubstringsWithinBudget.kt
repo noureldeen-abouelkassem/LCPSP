52f7c291ba088c8b6788336aba25aae1dcfa39d9
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-07-20 21:53:55 +0300

    Update file 7/20/2024, 9:53:54 PM

--- 1208-GetEqualSubstringsWithinBudget/1208-GetEqualSubstringsWithinBudget.kt
+++ 1208-GetEqualSubstringsWithinBudget/1208-GetEqualSubstringsWithinBudget.kt
@@ -1,14 +1,12 @@
-        var best = 0
-        for (right in s.indices) {
-            var costOfRight = abs(s[right]-t[right]).toInt()
-            totalCost+= costOfRight
-            while (totalCost > maxCost) {
-                var costOfLeft = abs(s[left]-t[left]).toInt()
-                totalCost-= costOfLeft
                 left++
             }
             best = maxOf(best, right-left +1)
         }
         return best
     }
+}
+
+
+
+
 "abcd"
