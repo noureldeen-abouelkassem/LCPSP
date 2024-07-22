073519cbeb930e9e3e5a4a5c75731c288bc71bff
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-07-22 14:34:03 +0300

    Update file 7/22/2024, 2:34:03 PM

--- 1208-GetEqualSubstringsWithinBudget/1208-GetEqualSubstringsWithinBudget.kt
+++ 1208-GetEqualSubstringsWithinBudget/1208-GetEqualSubstringsWithinBudget.kt
@@ -1,12 +1,14 @@
+class Solution {
+    fun equalSubstring(s: String, t: String, maxCost: Int): Int {
+        var left = 0
+        var totalCost = 0
+        var best = 0
+        for (right in s.indices) {
+            var costOfRight = abs(s[right]-t[right]).toInt()
+            totalCost+= costOfRight
+            while (totalCost > maxCost) {
+                var costOfLeft = abs(s[left]-t[left]).toInt()
+                totalCost-= costOfLeft
                 left++
             }
-            best = maxOf(best, right-left +1)
-        }
-        return best
-    }
-}
-
-
-
-
 "abcd"
