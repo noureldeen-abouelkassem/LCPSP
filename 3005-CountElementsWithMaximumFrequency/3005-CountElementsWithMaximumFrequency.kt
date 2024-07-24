679e9c943c2ed6be0d46a21b6853ef46a733d904
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-07-24 15:06:02 +0300

    Update file 7/24/2024, 3:06:02 PM

--- 3005-CountElementsWithMaximumFrequency/3005-CountElementsWithMaximumFrequency.kt
+++ 3005-CountElementsWithMaximumFrequency/3005-CountElementsWithMaximumFrequency.kt
@@ -1,14 +1,14 @@
-class Solution {
-    fun maxFrequencyElements(nums: IntArray): Int {
-        val map = hashMapOf<Int, Int>()
-        nums.forEach{
-            map[it] = map.getOrDefault(it, 0) + 1
         }
         var max = -1
         map.forEach{ key, value ->
-        }
-    }
             if(value > max)
                 max = value
+        }
         return map.filter{ it.value == max }.values.sum()
-[
+    }
+            map[it] = map.getOrDefault(it, 0) + 1
+        nums.forEach{
+        val map = hashMapOf<Int, Int>()
+    fun maxFrequencyElements(nums: IntArray): Int {
+class Solution {
+[1,2,2,3,1,4]
