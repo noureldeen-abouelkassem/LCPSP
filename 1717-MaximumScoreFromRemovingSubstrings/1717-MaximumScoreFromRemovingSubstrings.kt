378a8f158894ff08d863d0c1e9bfef40756bcb34
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-07-26 21:19:28 +0300

    Update file 7/26/2024, 9:19:28 PM

--- 1717-MaximumScoreFromRemovingSubstrings/1717-MaximumScoreFromRemovingSubstrings.kt
+++ 1717-MaximumScoreFromRemovingSubstrings/1717-MaximumScoreFromRemovingSubstrings.kt
@@ -1,10 +1,14 @@
-            if (stack2.isNotEmpty() && stack2.peek() == first && char == second) {
-                stack2.pop()
-                result += minOf(x, y)
-            } else stack2.push(char)
-        }
+class Solution {
+    fun maximumGain(s: String, x: Int, y: Int): Int {
+        val stack1 = Stack<Char>()
+        val stack2 = Stack<Char>()
+        val first = if (x > y) 'a' else 'b'
+        val second = if (x > y) 'b' else 'a'
+        var result = 0
 
-        return result
-    }
-}
+        s.forEach { char ->
+            if (stack1.isNotEmpty() && stack1.peek() == first && char == second) {
+                stack1.pop()
+                result += maxOf(x, y)
+            } else stack1.push(char)
 "cdbcbbaaabab"
