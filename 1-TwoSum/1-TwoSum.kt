4fc14233453713c1c27cc9bec66cbab7a6f1843b
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2024-07-30 22:15:33 +0300

    Update file 7/30/2024, 10:15:32 PM

--- 1-TwoSum/1-TwoSum.kt
+++ 1-TwoSum/1-TwoSum.kt
@@ -1,10 +1,14 @@
-Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â leftÂ +Â rightÂ ==Â targetÂ ->Â returnÂ intArrayOf(nums.indexOfFirst{itÂ ==Â left},Â nums.indexOfLast
-{itÂ ==Â right})
-Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â leftÂ +Â rightÂ >Â targetÂ ->Â rightPointer--
-Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â leftÂ +Â rightÂ <Â targetÂ ->Â leftPointer++
-Â Â Â Â Â Â Â Â Â Â Â Â }
-Â Â Â Â Â Â Â Â }
-Â Â Â Â Â Â Â Â returnÂ intArrayOf()
-Â Â Â Â }
-}
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val numsSorted = nums.sorted()
+        var leftPointer = 0
+        var rightPointer = nums.lastIndex
+        while(leftPointer < rightPointer){
+            val left = numsSorted[leftPointer]
+            val right = numsSorted[rightPointer]
+            println(left)
+            println(right)
+            when {
+                left + right == target -> return intArrayOf(nums.indexOfFirst{it == left}, nums.indexOfLast
+{it == right})
 [2,7,11,15]
