6e50c8fa30fed2f64b7ca361d53e559bd6c81d53
Nour El-Deen Abou El-Kassem <noureldeen.abouelkassem@gmail.com>
2025-06-17 13:01:58 +0300

    Time: 0 ms (100.00%) | Memory: 19.8 MB (53.86%) - LeetSync

--- 35-search-insert-position/search-insert-position.swift
+++ 35-search-insert-position/search-insert-position.swift
@@ -1,20 +1,17 @@
 class Solution {
     func searchInsert(_ nums: [Int], _ target: Int) -> Int {
-        var low = 0 
+        var low = 0
         var high = nums.count - 1
         while low <= high {
-            let middle = (low + high) / 2
-            
-            if nums[middle] == target {
-                return middle
+            let mid = (low + high) / 2
+            if nums[mid] == target {
+                return mid
             }
-            
-            if nums[middle] < target {
-                low = middle + 1
+            if nums[mid] < target {
+                low = mid + 1
             }
-            
-            if nums[middle] > target {
-                high = middle - 1
+            if nums[mid] > target {
+                high = mid - 1
             }
         }
         return low
